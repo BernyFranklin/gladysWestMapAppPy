33b827402108e9d9beacc23a2de5979f4be26a04
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-04-04 19:17:20 -0700

    Added equations for compute

--- src/gladysCompute.py
+++ src/gladysCompute.py
@@ -1,30 +1,35 @@
 import io
 
 import gladysSatellite as satellite
+import math
 
 """
 	Student: Gabriel Solomon
 	Module: gladysCompute
 	Description: This module does …
-	Status: gpsAverage   [ ]
-			distance     [ ]
+	Status: gpsAverage   [X]
+			distance     [X]
 			
 """
 
 
 def gpsAverage(x, y):
 	"""
-		document your function definition here. what does it do?
+		This function takes X and Y and gets its associated value for each satellite.
+		The values are added together and divided by the number of satellites [4]
+		returns Average
 	"""
 
 	"""
-		delete the remaining code *in this function* and replace it with
-		your own code. add more code to do what the assignment asks of you.
+		Look up in JSONs (x,y, lat) (x,y, long) (x,y, alt) (x,y, time)
 	"""
-
-	value = satellite.gpsValue(5, 6, "altitude")
-
-	average = value / 2
+	latitude  = satellite.gpsValue(x, y, "latitude")
+	longitude = satellite.gpsValue(x, y, "longitude")
+	altitude  = satellite.gpsValue(x, y, "altitude")
+	time      = satellite.gpsValue(x, y, "time")
+	#value = satellite.gpsValue(5, 6, "altitude")
+	average = (latitude + longitude + altitude + time)/4 
+	#average = value / 2
 
 	return average
 
@@ -39,9 +44,8 @@ def distance(current, destination):
 		delete the remaining code *in this function* and replace it with
 		your own code. add more code to do what the assignment asks of you.
 	"""
-	distance = gpsAverage(3, 4)
+	distance = math.sqrt((current**2) + (destination**2))
+	
 
 	return distance
 
-print(gpsAverage)
-print(distance)
